ecc967b0c2f28617a1383cbb01ec15091b2114a7
Andreas Borchsenius Westh <andreas@lundogbendsen.dk>
2016-12-13 14:37:19 +0100

    junit4 annotation and a new test with sample data

--- src/test/java/dk/dbc/kafka/KafkaConsumerProducerTest.java
+++ src/test/java/dk/dbc/kafka/KafkaConsumerProducerTest.java
@@ -1,13 +1,14 @@
 package dk.dbc.kafka;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Properties;
+import java.util.Scanner;
 
-import junit.framework.TestCase;
 import kafka.utils.SystemTime$;
 import org.I0Itec.zkclient.ZkClient;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -15,8 +16,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 import kafka.admin.AdminUtils;
 import kafka.admin.RackAwareMode;
@@ -26,6 +27,7 @@ import kafka.utils.TestUtils;
 import kafka.utils.ZKStringSerializer$;
 import kafka.utils.ZkUtils;
 import kafka.zk.EmbeddedZookeeper;
+
 
 
 /**
@@ -40,75 +42,77 @@ import kafka.zk.EmbeddedZookeeper;
  * http://kafka.apache.org/0100/javadoc/org/apache/kafka/clients/consumer/ConsumerRecord.html
  * http://kafka.apache.org/0100/javadoc/org/apache/kafka/clients/producer/ProducerRecord.html
  */
-public class KafkaConsumerProducerTest extends TestCase {
+public class KafkaConsumerProducerTest {
 
     private static final String ZKHOST = "127.0.0.1";
     private static final String BROKERHOST = "127.0.0.1";
     private static final String BROKERPORT = "9092";
     private static final String TOPIC = "test";
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public KafkaConsumerProducerTest(String testName )
-    {
-        super( testName );
-    }
+    private static final String TOPIC_JSON = "test_json";
+    EmbeddedZookeeper zkServer;
+    ZkClient zkClient;
+    ZkUtils zkUtils;
+    Properties brokerProps;
+    KafkaServer kafkaServer;
+    Properties producerProps;
+    Properties consumerProps;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( KafkaConsumerProducerTest.class );
-    }
-
-    /**
-     * Rigourous Test
-     */
-    public void testKafkaProducerIT()
-    {
-        assertTrue( true );
-    }
-
-
-    public void testProducer() throws InterruptedException, IOException {
+    @Before
+    public void prepare(){
 
         // setup Zookeeper
-        EmbeddedZookeeper zkServer = new EmbeddedZookeeper();
+        zkServer = new EmbeddedZookeeper();
         String zkConnect = ZKHOST + ":" + zkServer.port();
-        ZkClient zkClient = new ZkClient(zkConnect, 30000, 30000, ZKStringSerializer$.MODULE$);
-        ZkUtils zkUtils = ZkUtils.apply(zkClient, false);
+        zkClient = new ZkClient(zkConnect, 30000, 30000, ZKStringSerializer$.MODULE$);
+        zkUtils = ZkUtils.apply(zkClient, false);
 
         // setup Broker
-        Properties brokerProps = new Properties();
+        brokerProps = new Properties();
         brokerProps.setProperty("zookeeper.connect", zkConnect);
         brokerProps.setProperty("broker.id", "0");
-        brokerProps.setProperty("log.dirs", Files.createTempDirectory("kafka-").toAbsolutePath().toString());
+        try {
+            brokerProps.setProperty("log.dirs", Files.createTempDirectory("kafka-").toAbsolutePath().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         brokerProps.setProperty("listeners", "PLAINTEXT://" + BROKERHOST +":" + BROKERPORT);
         KafkaConfig config = new KafkaConfig(brokerProps);
 
-        KafkaServer kafkaServer = TestUtils.createServer(config, SystemTime$.MODULE$);  //Time mock = new MockTime();
+        kafkaServer = TestUtils.createServer(config, SystemTime$.MODULE$);  //Time mock = new MockTime();
 
         // create topic
         AdminUtils.createTopic(zkUtils, TOPIC, 1, 1, new Properties(), RackAwareMode.Disabled$.MODULE$);
 
         // setup producer
-        Properties producerProps = new Properties();
+        producerProps = new Properties();
         producerProps.setProperty("bootstrap.servers", BROKERHOST + ":" + BROKERPORT);
         producerProps.setProperty("key.serializer","org.apache.kafka.common.serialization.IntegerSerializer");
         producerProps.setProperty("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
-        KafkaProducer<Integer, byte[]> producer = new KafkaProducer<Integer, byte[]>(producerProps);
 
         // setup consumer
-        Properties consumerProps = new Properties();
+        consumerProps = new Properties();
         consumerProps.setProperty("bootstrap.servers", BROKERHOST + ":" + BROKERPORT);
         consumerProps.setProperty("group.id", "group0");
         consumerProps.setProperty("client.id", "consumer0");
         consumerProps.setProperty("key.deserializer","org.apache.kafka.common.serialization.IntegerDeserializer");
         consumerProps.setProperty("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
         consumerProps.put("auto.offset.reset", "earliest");  // to make sure the consumer starts from the beginning of the topic
+
+    }
+    /**
+     * Rigourous Test
+     */
+    @Test
+    public void testKafkaProducerIT()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testProducer() throws InterruptedException, IOException {
+
+        KafkaProducer<Integer, byte[]> producer = new KafkaProducer<Integer, byte[]>(producerProps);
+
         KafkaConsumer<Integer, byte[]> consumer = new KafkaConsumer<>(consumerProps);
         consumer.subscribe(Arrays.asList(TOPIC));
 
@@ -126,6 +130,58 @@ public class KafkaConsumerProducerTest extends TestCase {
         assertEquals(42, (int) record.key());
         assertEquals("test-message", new String(record.value(), StandardCharsets.UTF_8));
 
+    }
+
+
+    @Test
+    public void testJsonProducer() throws InterruptedException, IOException {
+
+        KafkaProducer<Integer, byte[]> producer = new KafkaProducer<Integer, byte[]>(producerProps);
+
+        KafkaConsumer<Integer, byte[]> consumer = new KafkaConsumer<>(consumerProps);
+        consumer.subscribe(Arrays.asList(TOPIC_JSON));
+
+        // send many messages
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("json-formatted-sample-data.txt").getFile());
+
+        int lines = 0;
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                lines++;
+                String line = scanner.nextLine();
+                //TODO: Make sure to use the ProducerRecord constructor that does not take partition Id
+                ProducerRecord<Integer, byte[]> rec = new ProducerRecord<>(TOPIC_JSON, line.getBytes(StandardCharsets.UTF_8));
+                producer.send(rec);
+
+            }
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        producer.close();
+
+        // starting consumer
+        ConsumerRecords<Integer, byte[]> records = consumer.poll(3000);
+        assertEquals(lines, records.count()); // number of lines in the file are the same as
+        Iterator<ConsumerRecord<Integer, byte[]>> recordIterator = records.iterator();
+
+        while(recordIterator.hasNext()){
+            ConsumerRecord<Integer, byte[]> record = recordIterator.next();
+            System.out.println("(" +record.offset() + ") ##### " + new String(record.value(), StandardCharsets.UTF_8));
+        }
+    }
+
+    // TODO use the log-tracer to consume
+    public void testLogTracer(){
+
+    }
+
+
+    @After
+    public void closeDown(){
         kafkaServer.shutdown();
         zkClient.close();
         zkServer.shutdown();
